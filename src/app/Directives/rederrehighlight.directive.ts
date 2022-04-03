1f65a8175018a280e36f9214ab6de86c70a531ed
iamkanishka <kanishkabc123@gmail.com>
2022-04-03 18:24:45 +0530

    Using Rendered2 to create the custom attribute directive to manipulate DOM in Angular

--- src/app/Directives/rederrehighlight.directive.ts
+++ src/app/Directives/rederrehighlight.directive.ts
@@ -1,15 +1,25 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRederrehighlight]'
 })
 export class RederrehighlightDirective implements OnInit {
 
-  constructor(private element:ElementRef, private rederer:Renderer2) { }
+  constructor(private element: ElementRef, private rederer: Renderer2) { }
 
   ngOnInit(): void {
-this.rederer.setStyle(this.element.nativeElement,'background-color','green')
-      
+ //Changing Style on Initializartion
+//    this.rederer.setStyle(this.element.nativeElement, 'background-color', 'green')
+}
+
+  @HostListener('mouseenter') onmouseover(event: Event) {
+ //Changing Style on mouseover
+   this.rederer.setStyle(this.element.nativeElement, 'background-color', 'green')
   }
+  @HostListener('mouseleave') onmouseleave(event: Event) {
+    //Changing Style on mouseleave
+      this.rederer.setStyle(this.element.nativeElement, 'background-color', 'yellow')
+     }
+   
 
 }

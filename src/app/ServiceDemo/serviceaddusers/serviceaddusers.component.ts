cdbb4e82045b77cd90591a0ac119aaeb896dbfde
iamkanishka <kanishkabc123@gmail.com>
2022-04-07 09:25:40 +0530

    Understand the Hierarchical Injection of the Services of Instances in the angular

--- src/app/ServiceDemo/serviceaddusers/serviceaddusers.component.ts
+++ src/app/ServiceDemo/serviceaddusers/serviceaddusers.component.ts
@@ -21,5 +21,14 @@ userStatus!:string
 this.serviceusersService.addUser(this.userName,this.userStatus)
   }
 
+  /**
+ * Note:- On Provider Injection Dependency injection
+ * Here we have three serviceusers, serviceuser, serviceadduser Compenents, and a user service 
+ * the Service User is imported in serviceusers, serviceuser, serviceadduser , since it is imported as a Proivder along with instance so it acts as separate intsance in teh separate compoenent, so we need to provide the serivce in the Hierarchical Manner, so that we can acces it in a unified instance
+ *  serviceusers, serviceuser, serviceadduser Compenents
+ * App-Module -  Hightest Level -> Unified Instance will Shared accross all over the  app
+ * App-Component - Next Highest Level -> same Instance will be Shared to all the Child Components but Services have differernt Instances
+ * Child Componenets -> Each One will having a Different instances
+ */
 
 }
--- src/app/ServiceDemo/serviceusers/serviceusers.component.ts
+++ src/app/ServiceDemo/serviceusers/serviceusers.component.ts
@@ -5,7 +5,7 @@ import { ServiceusersService } from '../../Services/serviceusers/serviceusers.se
   selector: 'app-serviceusers',
   templateUrl: './serviceusers.component.html',
   styleUrls: ['./serviceusers.component.scss'],
-  
+  //providers:[ServiceusersService]
 })
 export class ServiceusersComponent implements OnInit {
 
@@ -16,4 +16,14 @@ export class ServiceusersComponent implements OnInit {
    this.users =  this.userservice.users 
   }
 
+
+/**
+ * Note:- On Provider Injection Dependency injection
+ * Here we have three serviceusers, serviceuser, serviceadduser Compenents, and a user service 
+ * the Service User is imported in serviceusers, serviceuser, serviceadduser , since it is imported as a Proivder along with instance so it acts as separate intsance in teh separate compoenent, so we need to provide the serivce in the Hierarchical Manner, so that we can acces it in a unified instance
+ *  serviceusers, serviceuser, serviceadduser Compenents
+ * App-Module -  Hightest Level -> Unified Instance will Shared accross all over the  app
+ * App-Component - Next Highest Level -> same Instance will be Shared to all the Child Components but Services have differernt Instances
+ * Child Componenets -> Each One will having a Different instances
+ */
 }
